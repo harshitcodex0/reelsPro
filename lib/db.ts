e3a745624895e2ea1c1925960fcd5e0010cf7e4a
harshitcodex0 <harshit456987@hotmail.com>
2025-02-02 11:40:06 +0530

    Reels Upload project

---
+++ lib/db.ts
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose';
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if(!MONGODB_URI){
+    throw new Error('Please define the MONGODB_URI environment variable file');
+}
+
+let cached = global.mongoose;
