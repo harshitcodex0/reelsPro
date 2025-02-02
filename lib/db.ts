1e12e4ee52816ad261dc2f1949051f7b36c6d883
harshitcodex0 <harshit456987@hotmail.com>
2025-02-03 00:46:57 +0530

    .

--- lib/db.ts
+++ lib/db.ts
@@ -1,9 +1,37 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = process.env.MONGODB_URI!;
 
-if(!MONGODB_URI){
+if (!MONGODB_URI) {
     throw new Error('Please define the MONGODB_URI environment variable file');
 }
 
 let cached = global.mongoose;
+
+if (!cached) {
+    cached = global.mongoose = { conn: null, promise: null };
+}
+
+export async function connectToDatabase() {
+    if (cached.conn) {
+        return cached.conn;
+    }
+
+    if (!cached.promise) {
+        const opts = {
+            bufferCommands: true,
+            maxPoolSize: 10
+        };
+
+        cached.promise = mongoose.connect(MONGODB_URI, opts).then(() => mongoose.connection)
+    }
+
+    try {
+        cached.conn = await cached.promise;
+    } catch (error) {
+        cached.promise = null;
+        throw new Error("Check database file");
+        throw error;
+    }
+    return cached.conn;
+}
